077bc0145430214cf5620e765ef1aba88f586902
Casey Davidson <davidson@econw.com>
2015-04-06 22:13:20 -0700

    Add a second route.

--- quiver.py
+++ quiver.py
@@ -1,9 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def quiver():
     return render_template('quiver.html')
 
+@app.route('/plot/', methods=['GET', 'POST'])
+def plot():
+    print request.values
+    if request.method == 'POST':
+        equation = request.form.get('equation')
+    else:
+        equation = request.args.get('equation')
+    return render_template('plot.html', equation=equation)
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
