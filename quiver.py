926563a063866944755f6cb3c560180d8cbdcd67
Casey Davidson <davidson@econw.com>
2015-04-13 21:59:55 -0700

    Create separate DiffEquation class instead of having math within route.

--- quiver.py
+++ quiver.py
@@ -18,56 +18,94 @@ def quiver():
     '''Route for homepage'''
     return render_template('quiver.html')
 
-def prep_equation(equation_string):
-    # First do a regualar expression check to verify that they've actually entered an equation
-    match1 = re.match('^(([xy+\-*/()0-9. ]+|sin\(|cos\(|exp\(|log\()?)+$', equation_string)
-    match2 = re.match('^.*([xy]) *([xy]).*$', equation_string)
-    if match1 and not match2:
-        try:
-            equation = sympify(equation_string)
-            f = lambdify((x, y), equation)
-            return f, equation
-        except SympifyError:
-            pass
-    return None, None
+
+class DiffEquation(object):
+    '''
+    Class that contains equation information and, if the equation is valid,
+    prepares the plot.
+    '''
+    
+    def __init__(self, equation_string):
+        self.equation_string = equation_string
+        self.equation = None
+        self.compute_func = None
+        self.figure = None
+    
+    def regex_check(self):
+        '''A quick regular expression check to see that the input resembles an equation'''
+        match1 = re.match('^(([xy+\-*/()0-9. ]+|sin\(|cos\(|exp\(|log\()?)+$', self.equation_string)
+        match2 = re.match('^.*([xy]) *([xy]).*$', self.equation_string)
+        if match1 and not match2:
+            return True
+        return False
+    
+    def prep_equation(self):
+        '''
+        Attempt to convert the string to a SymPy function.
+        From there, use lambdify to generate a function that is efficient to compute
+        numerically.
+        '''
+        if self.regex_check():
+            try:
+                self.equation = sympify(self.equation_string)
+                self.compute_func = lambdify((x, y), self.equation)
+            except SympifyError:
+                pass
+    
+    def make_plot(self):
+        '''Draw the plot on the figure attribute'''
+        if self.compute_func:
+            xvals, yvals = np.arange(-10, 11, 1), np.arange(-10, 11, 1)
+            X, Y = np.meshgrid(xvals, yvals)
+            U, V = np.meshgrid(np.zeros(len(xvals)), np.zeros(len(yvals)))
+            
+            # Iterate through grid and compute function value at each point
+            # If value cannot be computed, default to 0
+            # If value can be computed, scale by sqrt of the magnitude
+            for i, a in enumerate(xvals):
+                for j, b in enumerate(yvals):
+                    dx = 1
+                    try:
+                        dy = self.compute_func(a, b)
+                        n = sqrt(dx + dy**2)
+                        dy /= sqrt(n)
+                        dx /= sqrt(n)
+                        U[j][i] = dx
+                        V[j][i] = dy
+                    except (ValueError, ZeroDivisionError):
+                        pass
+            
+            # Plot the values
+            self.figure = plt.Figure()
+            axes = self.figure.add_subplot(1,1,1)
+            axes.quiver(X, Y, U, V, angles='xy')
+            axes.axhline(color='black')
+            axes.axvline(color='black')
+            axes.set_title(r'Direction field for $\frac{dy}{dx} = %s$' % latex(self.equation), y=1.01)
+        
+    def write_data(self, output):
+        '''Write the data out as base64 binary'''
+        if self.figure:
+            canvas = FigureCanvas(self.figure)
+            self.figure.savefig(output, format='png')
+            output.seek(0)
+            return output.getvalue()
+        return None
+
 
 @app.route('/plot/', methods=['GET',])
 def plot():
     equation_string = request.args.get('equation')
-    f, equation = prep_equation(equation_string)
+    diff_equation = DiffEquation(equation_string)
+    diff_equation.prep_equation()
+    diff_equation.make_plot()
     
-    # If the equation is valid, compute the values for each value in the grid
-    if f:
-        xvals, yvals = np.arange(-10, 11, 1), np.arange(-10, 11, 1)
-        X, Y = np.meshgrid(xvals, yvals)
-        U, V = np.meshgrid(np.zeros(len(xvals)), np.zeros(len(yvals)))
-        for i, a in enumerate(xvals):
-            for j, b in enumerate(yvals):
-                dx = 1
-                try:
-                    dy = f(a, b)
-                    n = sqrt(dx + dy**2)
-                    dy /= sqrt(n)
-                    dx /= sqrt(n)
-                    U[j][i] = dx        
-                    V[j][i] = dy
-                except (ValueError, ZeroDivisionError):
-                    pass
-        
-        # Plot the values
-        fig = plt.Figure()
-        axes = fig.add_subplot(1,1,1)
-        axes.quiver(X, Y, U, V, angles='xy')
-        axes.axhline(color='black')
-        axes.axvline(color='black')
-        axes.set_title(r'Direction field for $\frac{dy}{dx} = %s$' % latex(equation), y=1.01)
+    # If plotting was successful, write plot out
+    if diff_equation.figure:
         
         # Write output to memory and add to response object
         output = StringIO.StringIO()
-        canvas = FigureCanvas(fig)
-        fig.savefig(output, format='png')
-        output.seek(0)
-        response = make_response(base64.b64encode(output.getvalue()))
+        response = make_response(base64.b64encode(diff_equation.write_data(output)))
         response.mimetype = 'image/png'
         return response
     else:
