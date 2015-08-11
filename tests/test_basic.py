d65e009b31a68d7f3f40bf012336dc8e9f324b21
Casey Davidson <davidson@econw.com>
2015-08-11 11:34:24 -0700

    Additional unit tests. Refs #2.

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -20,6 +20,26 @@ class FieldPlotterTestCase(unittest.TestCase):
         equation = sympy.sympify('2*x + 3*y')
         plotter.set_equation(equation)
         self.assertEqual(equation, plotter.equation)
+    
+    def test_set_equation_from_string(self):
+        equations = [
+            'x',
+            'y',
+            'x+y',
+            '2*x',
+            'x*y',
+        ]
+        plotter = FieldPlotter()
+        for equation_str in equations:
+            plotter.set_equation_from_string(equation_str)
+            sympy_eq = sympy.sympify(equation_str)
+            self.assertEqual(plotter.equation, sympy_eq)
+    
+    def test_set_equation_from_string_timeout(self):
+        equation_str = '9**9**9**9**9**9'
+        plotter = FieldPlotter()
+        plotter.set_equation_from_string(equation_str)
+        self.assertIsNone(plotter.equation)
 
 if __name__ == '__main__':
     unittest.main()
