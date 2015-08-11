b9c27063706bb0540f18fa5cc21fb3897971e20e
Casey Davidson <davidson@econw.com>
2015-08-10 17:08:48 -0700

    Add unit test for set_equation method. Refs #2.

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -4,6 +4,7 @@ import sympy
 
 from plot_equation import FieldPlotter
 
+
 class FieldPlotterTestCase(unittest.TestCase):
     
     def test_initialization(self):
@@ -13,6 +14,12 @@ class FieldPlotterTestCase(unittest.TestCase):
         equation = sympy.sympify('x+y')
         field_plotter_eq = FieldPlotter(equation)
         self.assertEqual(equation, field_plotter_eq.equation)
+    
+    def test_set_equation(self):
+        plotter = FieldPlotter()
+        equation = sympy.sympify('2*x + 3*y')
+        plotter.set_equation(equation)
+        self.assertEqual(equation, plotter.equation)
 
 if __name__ == '__main__':
     unittest.main()
